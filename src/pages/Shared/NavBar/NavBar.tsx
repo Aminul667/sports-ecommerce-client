9ddb618cc53e97fd7f71472be44f954e02eb53d4
aminul <rahathssn@gmail.com>
2024-09-23 17:11:15 +0600

    added components

---
+++ src/pages/Shared/NavBar/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <div>
+      <h2>This is NavBar Components</h2>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
